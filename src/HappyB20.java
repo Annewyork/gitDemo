bcabccbbb1fc4955e757f246b7e46572e9b524b6
Annewyork <alimnamazov@gmail.com>
2020-08-01 12:51:35 -0400

    add HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,7 +1,11 @@
 public class HappyB20 {
+
+
     public static void main(String[] args) {
 
    //always try
         System.out.println("if failure try again");
+
+        //Allahu Akbar
     }
 }
