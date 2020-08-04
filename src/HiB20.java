7806b74b56631c402473cb68de290bfdfea18cc4
Annewyork <alimnamazov@gmail.com>
2020-08-04 16:15:32 -0400

    update HiB20.java

--- src/HiB20.java
+++ src/HiB20.java
@@ -5,6 +5,21 @@ import java.util.Collections;
 public class HiB20 {
 
     public static void main(String[] args) {
-      //gg
+
+        String str = "ABCC";
+        String only ="";
+
+        for (int i = 0; i <= str.length()-1; i++){
+
+            char ch = str.charAt(i);
+            int ch1  = str.indexOf(ch);
+            int ch2 = str.lastIndexOf(ch);
+
+            if (ch1==ch2){
+                only+=ch;
+            }
+        }
+        System.out.println(only);
+
     }
 }
